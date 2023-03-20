ce13b2c60029cc4fd66b6038fe4ec17de30ad6b9
ssouz <souz.rs@outlook.com>
2023-03-20 11:54:52 -0300

    request parameter in url

--- topic2-1/src/main/java/com/souz/Topic21Application.java
+++ topic2-1/src/main/java/com/souz/Topic21Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Topic21Application {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(Topic21Application.class, args);
 	}
 
--- topic2-1/src/main/java/com/souz/controllers/HelloWorldController.java
+++ topic2-1/src/main/java/com/souz/controllers/HelloWorldController.java
@@ -3,17 +3,38 @@ package com.souz.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 @Controller
 public class HelloWorldController {
     @GetMapping("/test")
     @ResponseBody
-    public String printHello(){
+    public String printHelloTxt(){
         return "Hello! ";
     }
     @GetMapping("/test2")
     public String showHelloPage(Model model){
         model.addAttribute("message","hi");
+        model.addAttribute("rating",99);
+        model.addAttribute("report", "Some great news from the station");
+        return "HelloPage";
+    }
+    @GetMapping("/test3")
+    public ModelAndView printHello(){
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("message","Have a super day");
+        mv.addObject("rating",55);
+        mv.setViewName("HelloPage");
+        return mv;
+    }
+    @GetMapping("/test4")
+    public String showHelloPage4(@RequestParam("message") String message, Model model){
+        model.addAttribute("message",message);
+        model.addAttribute("rating",22);
+        model.addAttribute("report","some great news");
         return "HelloPage";
     }
 
