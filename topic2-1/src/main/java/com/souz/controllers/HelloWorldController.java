bc8d937f4f8be627fa6dd5388f0ebddccdc8dbc8
ssouz <souz.rs@outlook.com>
2023-03-21 10:59:43 -0300

    request parameter in url

--- topic2-1/src/main/java/com/souz/controllers/HelloWorldController.java
+++ topic2-1/src/main/java/com/souz/controllers/HelloWorldController.java
@@ -1,11 +1,15 @@
 package com.souz.controllers;
 
+import com.souz.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HelloWorldController {
@@ -37,5 +41,21 @@ public class HelloWorldController {
         model.addAttribute("report","some great news");
         return "HelloPage";
     }
+    @GetMapping("/people")
+    public String showFriends(Model model){
+
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person(0, "Sarah Johnson",28 ,145f));
+        people.add(new Person(1,"David Lee", 42 ,185f));
+        people.add(new Person(2,"Emily Chen",19 ,120f));
+        people.add(new Person(3,"Rachel Lee", 42, 150f));
+        people.add(new Person(4,"David Brown", 18, 160f));
+        people.add(new Person(5,"Lisa Wong", 29, 120));
+        System.out.println(people);
+       model.addAttribute("people", people);
+        return "printPeople";
+    }
+
 
 }
---
+++ topic2-1/src/main/java/com/souz/models/Person.java
@@ -0,0 +1,61 @@
+package com.souz.models;
+
+public class Person {
+    //add some properties, constructor and toString
+    private int id;
+    private String name;
+    private int age;
+    private float weight;
+
+
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", weight=" + weight +
+                '}';
+    }
+
+    public Person(int id, String name, int age, float weight) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}
