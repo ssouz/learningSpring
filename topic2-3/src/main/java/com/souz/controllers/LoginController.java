02f1f8090f45b4398651812ddd7ee9c90b1c176a
ssouz <souz.rs@outlook.com>
2023-03-26 12:22:25 -0300

    Add lombok and log4j2

--- topic2-3/src/main/java/com/souz/controllers/LoginController.java
+++ topic2-3/src/main/java/com/souz/controllers/LoginController.java
@@ -2,14 +2,16 @@ package com.souz.controllers;
 
 
 import com.souz.models.LoginModel;
+
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Log4j2
 @Controller
 @RequestMapping("/login")
 public class LoginController {
@@ -21,15 +23,13 @@ public class LoginController {
     }
     @PostMapping("/processLogin")
     public String processLogin(@Valid LoginModel loginModel, BindingResult bindingResult, Model model){
-        System.out.println("username: "+loginModel.getUsername());
-        System.out.println("password: "+loginModel.getPassword());
 
         if (bindingResult.hasErrors()){
             model.addAttribute("loginModel", loginModel);
-            System.out.println("fail");
+            log.info("error while trying to login with Username: {} and Password: {}",loginModel.getUsername(),loginModel.getPassword());
             return "LoginForm";
         }
-
+        log.info("successful login with Username: {} and Password: {}",loginModel.getUsername(),loginModel.getPassword());
         model.addAttribute("loginModel", loginModel);
         return "LoginResults";
     }
--- topic2-3/src/main/java/com/souz/models/LoginModel.java
+++ topic2-3/src/main/java/com/souz/models/LoginModel.java
@@ -2,44 +2,22 @@ package com.souz.models;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class LoginModel {
-    @NotNull(message="Username is required")
+    @NotNull
     @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters")
     private String username;
+
+    
     @NotNull
     @Size(min = 3, max = 15, message = "Password must be between 3 and 15 characters")
     private String password;
 
-    public LoginModel() {
-    }
-
-    public LoginModel(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "LoginModel{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
